1aae9bf0cf6084731d0a74966f9e7265d4d3eafe
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-02-20 21:44:00 +0900

    (2026.2.20) 다형성

---
+++ Program/Message.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Message.h"
+
+void Message::Send(int frequency)
+{
+	cout << "frequency : " << frequency << endl;
+}
+
+void Message::Send(const char* content)
+{
+	cout << content << endl;
+}
+
+void Message::Send(const char* content, int frequency)
+{
+	cout << content << "frequency : " << frequency << endl;
+}
