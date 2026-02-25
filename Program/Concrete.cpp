1d70c1c667818088ebdba3125b369010d576c4e0
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-02-25 21:45:05 +0900

    (2026.2.25) 추상화

---
+++ Program/Concrete.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "Concrete.h"
+
+void Concrete::Describe()
+{
+	cout << "Concreate Information" << endl;
+}
---
+++ Program/Stone.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Block.h"
+class Stone : public Block
+{
+public:
+	virtual void Describe() override;
+};
+
